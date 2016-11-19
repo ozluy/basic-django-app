ad594fcf1b12dc619dfeec6a5a980084cfcf0fb2
Yusuf Özlü <ozluyusuf@gmail.com>
2016-11-20 01:21:58 +0300

    add teplates and static files

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
-def index(response):
-    return HttpResponse('<h1>Hello World! </h1>')
+def index(request):
+    return render(request, 'index.html', {'persons': persons})
+
+
+class Person:
+    def __init__(self, name, age, experience, title, location, avatar):
+        self.name = name
+        self.experience = experience
+        self.age = age
+        self.title = title
+        self.location = location
+        self.avatar = avatar
+
+persons = [
+    Person('Jon Snow', 25, 2, 'Front-end Developer', 'Istanbul', 'jon_snow.png'),
+    Person('Ned Stark', 46, 14, 'Software Architect', 'Ankara', 'ned_stark.png'),
+    Person('Jaime Lannister', 28, 6, 'UI Designer', 'Izmir', 'jaime_lannister.png'),
+    Person('Khal Drogo', 30, 8, 'Full-stack Developer', 'Antalya', 'khal_drogo.png'),
+]
