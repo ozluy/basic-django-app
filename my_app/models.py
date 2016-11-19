2f27fbe9cedf882526cc840d9a516693ce43f6c6
Yusuf Özlü <ozluyusuf@gmail.com>
2016-11-20 02:34:25 +0300

    add models and setup admin

--- my_app/models.py
+++ my_app/models.py
@@ -3,3 +3,22 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+"""
+Person(
+name = 'Daenerys Targaryen',
+age = 25,
+experience_year =  2,
+title = 'Back-end Developer',
+location ='Trabzon',
+avatar ='daenerys_targaryen.png')
+"""
+
+class Person(models.Model):
+    name = models.CharField(max_length = 100)
+    experience_year = models.IntegerField()
+    age = models.IntegerField()
+    title = models.CharField(max_length = 100)
+    location = models.CharField(max_length = 100)
+    avatar = models.CharField(max_length = 500)
+    def __str__(self):
+        return self.name
--- my_app/views.py
+++ my_app/views.py
@@ -1,21 +1,6 @@
 from django.shortcuts import render
+from .models import Person
 # Create your views here.
 def index(request):
+    persons = Person.objects.all()
     return render(request, 'index.html', {'persons': persons})
-
-
-class Person:
-    def __init__(self, name, age, experience, title, location, avatar):
-        self.name = name
-        self.experience = experience
-        self.age = age
-        self.title = title
-        self.location = location
-        self.avatar = avatar
-
-persons = [
-    Person('Jon Snow', 25, 2, 'Front-end Developer', 'Istanbul', 'jon_snow.png'),
-    Person('Ned Stark', 46, 14, 'Software Architect', 'Ankara', 'ned_stark.png'),
-    Person('Jaime Lannister', 28, 6, 'UI Designer', 'Izmir', 'jaime_lannister.png'),
-    Person('Khal Drogo', 30, 8, 'Full-stack Developer', 'Antalya', 'khal_drogo.png'),
-]
